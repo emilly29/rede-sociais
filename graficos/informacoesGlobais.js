7bf0bfe48b7dd97b802763b58df25411cc9b5a93
emilly29 <103429686+emilly29@users.noreply.github.com>
2024-10-11 09:31:54 -0300

    .

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -8,4 +8,7 @@ paragrafo.classList.add('graficos-container_texto')
 paragrafo.innerHTML = ` Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_conectadas} estao conectadas em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas.`
 
 const container = document.getElementById('geaficos-container')
+container.appendChild(paragrafo)
 }
+
+vizualizainformacoesGlobais()
