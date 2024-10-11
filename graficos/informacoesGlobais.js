cb5698fda72311d7c7b851f353a9ca094c39b84a
emilly29 <103429686+emilly29@users.noreply.github.com>
2024-10-11 09:30:08 -0300

    .

---
+++ graficos/informacoesGlobais.js
@@ -0,0 +1,11 @@
+const url=https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json
+
+async function vizualizainformacoesGlobais() {
+const res = await fetch(url)
+const dados = await res.json()
+const paagrafo = document.createElement('p')
+paragrafo.classList.add('graficos-container_texto')
+paragrafo.innerHTML = ` Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_conectadas} estao conectadas em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas.`
+
+const container = document.getElementById('geaficos-container')
+}
